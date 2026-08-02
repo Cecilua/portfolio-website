54a14030e49927b6cdc619f607b4a4ad48b530b1
Cecilua <kuntzece@gmail.com>
2026-08-02 15:17:05 +1200

    feat: made responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route } from "react-router"
+import NotFound from "@/components/NotFound.tsx"
+import Home from "@/components/Home.tsx"
+import NavBar from "@/components/NavBar.tsx"
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,103 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useMediaQuery } from "@/components/use-media-query.ts"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import {Menu, X } from "lucide-react"
+import { Link } from "react-router"
+
+export default function NavBar() {
+  const isDesktop = useMediaQuery("(min-width: 48rem)");
+
+  return isDesktop ? (
+    <div className="flex flex-col items-center justify-center gap-3 bg-popover/70 p-5">
+      <img
+        className="rounded-full md:h-35 md:w-35 lg:h-40 lg:w-40 xl:h-50 xl:w-50"
+        src="https://avatars.githubusercontent.com/u/65154517"
+      ></img>
+      <h1>Cecilia Kuntze</h1>
+      <div className="grid w-full grid-cols-[1fr_auto_1fr] items-center">
+        <a
+          href="https://github.com/Cecilua"
+          className="text-underline flex flex-row items-center gap-3 justify-self-end pr-3 underline hover:text-accent"
+        >
+          <i className="bi bi-github text-2xl"></i>
+          Github
+        </a>
+        <span>|</span>
+        <a
+          href="https://www.linkedin.com/in/cecilia-kuntze-a208a43ab/"
+          className="text-underline flex flex-row items-center gap-3 justify-self-start pl-3 underline hover:text-accent"
+        >
+          <i className="bi bi-linkedin text-2xl"></i>
+          LinkedIn
+        </a>
+      </div>
+      <div className="grid w-full grid-cols-[1fr_auto_auto_auto_1fr] items-center text-xl">
+        <Link
+          className="flex justify-self-end pr-10 hover:text-accent"
+          to="/"
+        >
+          Home
+        </Link>
+        <span>|</span>
+        <Link
+          className="flex justify-self-center px-10 hover:text-accent"
+          to="/"
+        >
+          Home
+        </Link>
+        <span>|</span>
+        <Link className="flex justify-self-start pl-10 hover:text-accent" to="/">
+          Home
+        </Link>
+      </div>
+    </div>
+  ) : (
+    <div className="flex w-full flex-row items-center gap-3 bg-popover/70 p-5">
+      <Drawer swipeDirection={"left"}>
+        <DrawerTrigger>
+          <Menu size={35} />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader className="text-center font-sans">
+            <DrawerClose>
+              <X size={35} />
+            </DrawerClose>
+            <DrawerTitle>Cecilia Kuntze</DrawerTitle>
+            <DrawerDescription>
+              3rd Year Software Engineering Student
+            </DrawerDescription>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
+      <Avatar size="lg">
+        <AvatarImage src="https://avatars.githubusercontent.com/u/65154517" />
+        <AvatarFallback>CK</AvatarFallback>
+      </Avatar>
+      <div className="flex grow">Cecilia Kuntze</div>
+      <a
+        href="https://github.com/Cecilua"
+        className="text-underline flex flex-row items-center gap-3 justify-self-end pr-3 underline hover:text-accent"
+      >
+        <i className="bi bi-github text-xl"></i>
+        Github
+      </a>
+      <span>|</span>
+      <a
+        href="https://www.linkedin.com/in/cecilia-kuntze-a208a43ab/"
+        className="text-underline flex flex-row items-center gap-3 justify-self-start pl-3 underline hover:text-accent"
+      >
+        <i className="bi bi-linkedin text-xl"></i>
+        LinkedIn
+      </a>
+    </div>
+  )
+}
