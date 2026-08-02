f2c37a720433322fa59ace582ffcc45c2377ffcb
Cecilua <kuntzece@gmail.com>
2026-08-02 20:01:19 +1200

    feat: testing pages for the first time

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio-website/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
